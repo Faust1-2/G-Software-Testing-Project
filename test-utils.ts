e04b2565f1be27ff44122e2ba363cfc90be76122
faustin <faustin.dewas@efrei.net>
2024-01-24 12:28:20 +0100

    push managament tests

---
+++ test-utils.ts
@@ -0,0 +1,90 @@
+import {Page} from "@playwright/test";
+
+
+export const baseURL= "https://g.hr.dmerej.info/";
+export function _(path: string) {
+    return baseURL + path;
+}
+
+export type Employee = {
+    name: string;
+    email: string;
+    addressLine1: string;
+    addressLine2: string;
+    city: string;
+    zipCode: string;
+    hiringDate: string;
+    job: string;
+
+};
+
+export function getRandomString(length: number) {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        result += charset.charAt(randomIndex);
+    }
+    return result;
+}
+
+export function getRandomEmail() {
+    const username = getRandomString(8);
+    const domain = getRandomString(5) + ".com";
+    return `_${username}@_${domain}`;
+}
+
+// Function to fill form with a wrong email
+export async function fillAddUserWithTestData(page: Page, testData: Partial<Employee>) {
+    const idName = page.getByRole("textbox", {name: "name"});
+    const idEmail = page.getByRole("textbox", {name: "email"});
+    const idAddressLine1 = page.getByRole("textbox", {name: "address_line1"});
+    const idAddressLine2 = page.getByRole("textbox", {name: "address_line2"});
+    const idCity = page.getByRole("textbox", {name: "city"});
+    const idZipCode = page.getByRole("textbox", {name: "zip_code"});
+    const idHiringDate = page.getByRole("textbox", {name: "hiring_date"});
+    const idJobTitle = page.getByRole("textbox", {name: "job_title"});
+
+    const name = testData.name ?? getRandomString(10);
+    const email = testData.email ?? getRandomEmail();
+    const addressLine1 = testData.addressLine1 ?? getRandomString(15);
+    const addressLine2 = testData.addressLine2 ?? getRandomString(10);
+    const city = testData.city ?? getRandomString(8);
+    const zipCode = testData.zipCode ?? String(Math.floor(Math.random() * 90000) + 10000);
+    const hiringDate = testData.hiringDate ?? "2020-12-12";
+    const job = testData.job ?? getRandomString(10);
+
+    await idName.fill(name);
+    await idEmail.fill(email);
+    await idAddressLine1.fill(addressLine1);
+    await idAddressLine2.fill(addressLine2);
+    await idCity.fill(city);
+    await idZipCode.fill(zipCode);
+    await idHiringDate.fill(hiringDate);
+    await idJobTitle.fill(job);
+
+    return {
+        name,
+        email,
+        addressLine1,
+        addressLine2,
+        city,
+        zipCode,
+        hiringDate,
+        job
+    }
+}
+
+export async function addUser(page: Page, testData: Partial<Employee>) {
+    await page.goto(_("add_employee"));
+    const user = await fillAddUserWithTestData(page, testData);
+    await page.locator("button").getByText("Add").click();
+    await page.waitForURL(_("employees"));
+    return user;
+}
+
+export async function resetDB(page: Page) {
+    await page.goto(_("reset_db"));
+    await page.locator("button").getByText("Proceed").click();
+    await page.waitForURL(_(""));
+}
---
+++ tests/team-management.test.ts
@@ -0,0 +1,111 @@
+import { test, expect } from '@playwright/test';
+import {  describe } from 'node:test';
+import { Employee, _, addUser } from '../test-utils';
+import dayjs from 'dayjs';
+
+test.describe('Team management tests', () => {
+  test.describe('Create a team', () => {
+
+    test.beforeEach(async ({ page }) => {
+      await page.goto(_('add_team'));
+    })
+
+    test('A team should be added', async ({ page }) => {
+      const teamName = 'Newteam';
+      const field = page.getByPlaceholder('Name');
+      await field.fill(teamName);
+      await field.press('Enter');
+
+      await page.goto(_('teams'));
+      await page.waitForURL(_('teams'));
+
+      const texts = await page.getByRole('table').allInnerTexts();
+      expect(texts[0]).toContain(teamName);
+    });
+
+    test('Should not allow a team to be created if the name is used', async ({ page }) => {
+      const teamName = 'DuplicatedTeam'
+      const first_add = page.getByPlaceholder('Name');
+      await first_add.fill(teamName);
+      await first_add.press('Enter')
+
+      await page.goto(_('add_team'));
+
+      const second_add = page.getByPlaceholder('Name');
+      await second_add.fill(teamName);
+      await second_add.press('Enter')
+
+      expect(page.url()).toBe('https://g.hr.dmerej.info/add_team');
+      expect(await page.waitForSelector('.invalid-feedback')).toBeDefined();
+    });
+  });
+
+  // test.describe('Add employee to a team', () => {
+  //   const team = 'EmployeeTeam';
+  //   const employeeData: Employee = {
+  //     name: 'Employee in a team',
+  //     email: 'emp.in@a.team',
+  //     addressLine1: 'company',
+  //     addressLine2: 'building',
+  //     city: 'company city',
+  //     hiringDate: dayjs().toString(),
+  //     job: 'Being an employee (in a team)',
+  //     zipCode: '42150'
+  //   }
+
+  //   test.beforeEach(async ({ page }) => {
+  //     await addUser(page, employeeData);
+  //     await page.goto(_('add_team'));
+  //     const field = page.getByPlaceholder('Name');
+  //     await field.fill(team);
+  //     await field.press('Enter');
+  //     await page.goto('employees');
+  //   });
+
+  //   test('it should add an employee to a team', async({ page }) => {
+  //     const locator = page.locator('tr', { hasText: employeeData.name }).locator('.btn', { hasText: 'Edit'});
+  //     console.log(await locator.innerHTML());
+  //   });
+  // });
+
+  test.describe('List all teams', () => {
+    const teams = ['team1', 'team2', 'team3'];
+
+    test.beforeEach(async ({ page }) => {
+      for (const team of teams) {
+        await page.goto(_('add_team'));
+        const field = page.getByPlaceholder('Name');
+        await field.fill(team);
+        await field.press('Enter');
+      };
+      await page.goto(_('teams'));
+    });
+
+    test('It should list all teams', async ({ page }) => {
+      const texts = await page.getByRole('table').allInnerTexts();
+
+      for (const team of teams) {
+        expect(texts[0]).toContain(team);
+      }
+    });
+  });
+
+  describe('Delete a team', () => {
+    const team = 'teamToDelete';
+
+    test.beforeEach(async ({ page }) => {
+      await page.goto(_('add_team'));
+      const field = page.getByPlaceholder('Name');
+      await field.fill(team);
+      await field.press('Enter');
+      await page.goto(_('teams'));
+    });
+
+    test('It should delete the seletected team', async ({ page }) => {
+      const locator = page.locator('tr', { hasText: team }).locator('.btn');
+      await locator.click();
+      await page.getByRole('button').click();
+      expect((await page.getByRole('table').allInnerTexts())[0]).not.toContain(team);
+    });
+  });
+});
